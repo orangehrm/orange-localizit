71a11389295e538095097347eeacc4b2e1535e74
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-14 12:45:17 +0000

    Adding Pages

--- trunk/apps/localizit/modules/authentication/templates/indexSuccess.php
+++ trunk/apps/localizit/modules/authentication/templates/indexSuccess.php
@@ -1,24 +1,33 @@
-<h1>Users List</h1>
-
-<table>
-  <thead>
-    <tr>
-      <th>User</th>
-      <th>Login name</th>
-      <th>Password</th>
-      <th>User type</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($users as $user): ?>
-    <tr>
-      <td><a href="<?php echo url_for('authentication/show?user_id='.$user->getUserId()) ?>"><?php echo $user->getUserId() ?></a></td>
-      <td><?php echo $user->getLoginName() ?></td>
-      <td><?php echo $user->getPassword() ?></td>
-      <td><?php echo $user->getUserTypeId() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-
-  <a href="<?php echo url_for('authentication/new') ?>">New</a>
+<div class="addLabelDiv loginForm" >
+    <form action="<?php echo url_for('@sign_in'); ?>" method="post" id="sign_in_form" name="sign_in_form">
+        <div class="formHeading">
+            Orange-Localizit
+        </div>
+        <table>
+            <tr>
+                <td>
+                    <?php echo $addSignInForm['login_name']->renderLabel('User Name') ?>
+                </td>
+                <td>
+                    <?php echo $addSignInForm['login_name']->render() ?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <?php echo $addSignInForm['password']->renderLabel('Password') ?>
+                </td>
+                <td>
+                    <?php echo $addSignInForm['password']->render() ?>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <input type="button" name="login_label" id="login_label" value="Login" />
+      
+                    <input type="button" name="cancel_label" id="cancel_label" value="Cancel" />
+                </td>
+            </tr>
+        </table>
+    </form>
+</div>
