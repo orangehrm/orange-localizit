8c8c9f960cd6b4b2fc4e70007f09c302576498f4
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-14 12:50:53 +0000

    user DAO & Srvice

---
+++ apps/localizit/lib/model/authentication/dao/AuthenticationDao.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * AuthenticationDao for User operation
+ *
+ * @author Chameera S
+ */
+class AuthenticationDao extends BaseDao {
+
+    /**
+     * get User object By name
+     * @param string $userName
+     * @returns User Object
+     * @throws DaoException
+     */
+    public function getUserByName($userName) {
+        try {
+            $q = Doctrine_Query :: create()
+                    ->from('User u')
+                    ->where('u.login_name = ?', $userName);
+
+            return $q->fetchOne();
+
+        } catch(Exception $e) {
+            throw new DaoException($e->getMessage());
+        }
+    }
+}
+
---
+++ test/model/service/AuthenticationServiceTest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ *
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ *
+*/
+
+/**
+ * Authentication Service Test class
+ *
+ * @author Chameera S
+ */
+require_once 'PHPUnit/Framework.php';
+
+class AuthenticationServiceTest  extends  PHPUnit_Framework_TestCase {
+
+    private $testCases;
+    private $authenticationService;
+    private $authenticationDao	;
+
+    /**
+     * PHPUnit setup function
+     */
+    public function setup() {
+        $this->authenticationService	=	new AuthenticationService();
+        $this->testCases 	= sfYaml::load(sfConfig::get('sf_test_dir') . '/fixtures/authentication/authentication.yml');
+
+    }
+
+    /**
+     * Test Get User
+     *
+     */
+    public function testGetUserByName() {
+        $this->authenticationDao		=	$this->getMock('AuthenticationDao');
+        $this->authenticationDao->expects($this->once())
+                ->method('getUserByName')
+                ->will($this->returnValue(Doctrine_Collection));
+
+        $this->authenticationService->setAuthenticationDao( $this->authenticationDao );
+
+        $result 	=	$this->authenticationService->getUserByName();
+        $this->assertTrue(true);
+    }
+}
