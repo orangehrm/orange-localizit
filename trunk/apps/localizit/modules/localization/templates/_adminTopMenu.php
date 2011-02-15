a8e2a92b056bc9f248e288f1e11a2f9ea98774da
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-15 12:19:24 +0000

    Modifications

--- trunk/apps/localizit/modules/localization/templates/_adminTopMenu.php
+++ trunk/apps/localizit/modules/localization/templates/_adminTopMenu.php
@@ -1,3 +1,13 @@
-<?php
-echo link_to('Logout', 'authentication/logout');
-?>
+<table>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td class="logout">
+            <?php
+            echo link_to('Logout', 'authentication/logout');
+            ?>
+
+        </td>
+    </tr>
+</table>
