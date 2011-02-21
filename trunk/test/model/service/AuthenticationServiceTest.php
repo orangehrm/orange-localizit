e5dae31d681fe0dd0822796338639faa11c68fc4
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-21 07:04:39 +0000

    top menu

--- trunk/test/model/service/AuthenticationServiceTest.php
+++ trunk/test/model/service/AuthenticationServiceTest.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
- *
- * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
- * all the essential functionalities required for any enterprise.
- * Copyright (C) 2006 OrangeHRM Inc., http://www.orangehrm.com
- *
- * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with this program;
- * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA
- *
- */
-
 /**
  * Authentication Service Test class
  *
@@ -47,15 +27,24 @@ class AuthenticationServiceTest extends PHPUnit_Framework_TestCase {
      */
     public function testGetUserByName() {
         foreach ($this->testCases['User'] as $key => $testCase) {
+            // instantiate User object
+            $user = new User();
+            $user->login_name = $testCase['login_name'];
+            $user->password = md5($testCase['password']);
+
             $this->authenticationDao = $this->getMock('AuthenticationDao');
             $this->authenticationDao->expects($this->once())
                     ->method('getUserByName')
-                    ->will($this->returnValue(Doctrine_Collection));
+                    ->will($this->returnValue($user));
 
             $this->authenticationService->setAuthenticationDao($this->authenticationDao);
 
             $result = $this->authenticationService->getUserByName($testCase['login_name']);
-            $this->assertTrue(true);
+
+            $this->assertTrue($result instanceof User);
+
+            //check passwords
+            $this->assertEquals($result->password, md5($testCase['password']));
         }
     }
 
