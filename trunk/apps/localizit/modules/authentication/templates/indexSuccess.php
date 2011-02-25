f21522ab3339b717423ac411ebf5bb4ea3306e2b
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-25 08:24:48 +0000

    UI Modifications

--- trunk/apps/localizit/modules/authentication/templates/indexSuccess.php
+++ trunk/apps/localizit/modules/authentication/templates/indexSuccess.php
@@ -1,10 +1,11 @@
+<div class="outerBorder loginBorderOuterFrame">
 <div class="addLabelDiv loginForm" >
     <form action="<?php echo url_for('@sign_in'); ?>" method="post" id="sign_in_form" name="sign_in_form">
         <?php echo $signInForm->renderHiddenFields(); ?>
         <div class="mainLogo">
             <?php echo __("orange_localizit", null, 'authenticationMessages'); ?>
         </div>
-        <table class="mediumText">
+        <table class="mediumText loginFormBackGround">
             <?php $globalErrors=$signInForm->getGlobalErrors();
             if(count($globalErrors)>0) {
                 foreach ($globalErrors as $name => $error) { ?>
@@ -43,7 +44,7 @@
                 <td>&nbsp;</td>
                 <td class="addDotLinetoRight">
                     <input type="button" name="login_label" id="login" class="button normalText" value="<?php echo __('login', null , 'authenticationMessages') ?>" />
-                    <input type="button" name="cancel_label" id="cancel_label" class="button normalText" value="<?php echo __('cancel', null , 'authenticationMessages') ?>" />
+                    <input type="reset" name="cancel_label" id="cancel_label" class="button normalText" value="<?php echo __('cancel', null , 'authenticationMessages') ?>" />
                 </td>
                 <?php if ($signInForm['password']->hasError() || $signInForm['loginName']->hasError()) { ?>
                 <td class="addDotLinetoRight">&nbsp;</td>
@@ -52,4 +53,4 @@
         </table>
     </form>
 </div>
-
+</div>
