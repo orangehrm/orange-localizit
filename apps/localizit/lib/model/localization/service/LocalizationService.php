f4646287f666bd45f5ac7014eba4f5ed8124f72c
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-02-25 06:33:38 +0000

    xml output formatted

--- apps/localizit/lib/model/localization/service/LocalizationService.php
+++ apps/localizit/lib/model/localization/service/LocalizationService.php
@@ -269,13 +269,57 @@ XML;
 
             $languageFile = sfConfig::get('sf_web_dir')."/language_files/messages." . $targetLanguageLabel . ".xml";
             $fh = fopen($languageFile, 'w') or die("can't open file");
-            $out = fwrite($fh, $xml->saveXML());
+            $formatted = $this->formatXmlString($xml->saveXML());
+            $out = fwrite($fh, $formatted);
             fclose($fh);
 
             return TRUE;
         } catch (Exception $exc) {
             throw new ServiceException($exc->getMessage(), $exc->getCode());
         }
+    }
+
+    /**
+     * formats the xml
+     * @param $xml
+     * @returns String
+     */
+    function formatXmlString($xml) {
+
+        // add marker linefeeds to aid the pretty-tokeniser (adds a linefeed between all tag-end boundaries)
+        $xml = preg_replace('/(>)(<)(\/*)/', "$1\n$2$3", $xml);
+
+        // now indent the tags
+        $token = strtok($xml, "\n");
+        $result = ''; // holds formatted version as it is built
+        $pad = 0; // initial indent
+        $matches = array(); // returns from preg_matches()
+        // scan each line and adjust indent based on opening/closing tags
+        while ($token !== false) :
+
+            // test for the various tag states
+            // 1. open and closing tags on same line - no change
+            if (preg_match('/.+<\/\w[^>]*>$/', $token, $matches)) :
+                $indent = 0;
+            // 2. closing tag - outdent now
+            elseif (preg_match('/^<\/\w/', $token, $matches)) :
+                $pad--;
+            // 3. opening tag - don't pad this one, only subsequent tags
+            elseif (preg_match('/^<\w[^>]*[^\/]>.*$/', $token, $matches)) :
+                $indent = 1;
+            // 4. no indentation needed
+            else :
+                $indent = 0;
+            endif;
+
+            // pad the line with the required number of leading spaces
+            $line = str_pad($token, strlen($token) + $pad, ' ', STR_PAD_LEFT);
+            $result .= $line . "\n"; // add to the cumulative result, with linefeed
+            $token = strtok("\n"); // get the next token
+            $pad += $indent; // update the pad size for subsequent lines
+        endwhile;
+
+        return $result;
     }
 
     /**
