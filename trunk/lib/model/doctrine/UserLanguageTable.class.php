e265238223ea0c132e78c4b68221031803bb4153
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-02 11:59:30 +0000

    Adding Table

---
+++ trunk/lib/model/doctrine/UserLanguageTable.class.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class UserLanguageTable extends Doctrine_Table
+{
+    
+    public static function getInstance()
+    {
+        return Doctrine_Core::getTable('UserLanguage');
+    }
+}
