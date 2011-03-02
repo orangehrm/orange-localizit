6d924b155ed63350a8a485257a1437cfd6aabcb0
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-02 08:35:53 +0000

    user role

---
+++ trunk/apps/localizit/lib/model/authentication/userrole/NormalUserRoleDecorator.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ *
+ * OrangeHRM is a comprehensive Human Resource Management (HRM) System that captures
+ * all the essential functionalities required for any enterprise.
+ * Copyright (C) 2011 OrangeHRM Inc., http://www.orangehrm.com
+ *
+ * OrangeHRM is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * OrangeHRM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ *
+ */
+
+class NormalUserRoleDecorator implements RoleDecorator {
+
+    public $user;
+
+    /**
+     * Get User
+     * @return unknown_type
+     */
+    public function getUser() {
+        return $this->user;
+    }
+
+    /**
+     * Set User
+     * @param $user
+     * @return unknown_type
+     */
+    public function setUser($user) {
+        $this->user = $user;
+    }
+
+    /**
+     *
+     * check ablity to manage users
+     */
+    public function isAllowedToManageUser() {
+        return false;
+    }
+
+    /**
+     * @author Chameera Senarathna
+     * @return Allowed Language list
+     */
+    public function getAllowedLanguageList() {
+
+        return null;
+    }
+
+    public function isAllowedToDownloadDirectory() {
+
+        return true;
+    }
+
+}
---
+++ trunk/test/AllUserRole.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Orange-localizit  is a System that transalate text into a any language.
+ * Copyright (C) 2011 Orange-localizit Inc., http://www.orange-localizit.com
+ *
+ * Orange-localizit is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * Orange-localizit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA
+ */
+
+require_once 'PHPUnit/Framework.php';
+define('ROOT_PATH', dirname(__FILE__) . '/../../');
+define('SF_APP_NAME', 'localizit');
+define('SF_ENV', 'test');
+define('SF_CONN', 'doctrine');
+
+if (!defined('TEST_ENV_CONFIGURED')) {
+
+    require_once(dirname(__FILE__) . '/../config/ProjectConfiguration.class.php');
+    AllTests::$configuration = ProjectConfiguration::getApplicationConfiguration(SF_APP_NAME, SF_ENV, true);
+    sfContext::createInstance(AllTests::$configuration);
+
+    define('TEST_ENV_CONFIGURED', TRUE);
+}
+
+class AllTests {
+
+    public static $configuration = null;
+    public static $databaseManager = null;
+    public static $connection = null;
+
+    protected function setUp() {
+
+        if (self::$configuration) {
+            // initialize database manager
+            self::$databaseManager = new sfDatabaseManager(self::$configuration);
+            self::$databaseManager->loadConfiguration();
+
+            if (SF_CONN != '') {
+                self::$connection = self::$databaseManager->getDatabase(SF_CONN);
+            }
+        }
+
+    }
+
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('PHPUnit');
+
+    	// execute dao unit tests
+    	$coredir = new DirectoryIterator(dirname(__FILE__). '/model/userrole/');
+        while ($coredir->valid()) {
+            if (strpos( $coredir, 'Test.php' ) !== false) {
+                $suite->addTestFile(  dirname(__FILE__). '/model/userrole/'. $coredir );
+            }
+            $coredir->next();
+        }
+
+        return $suite;
+    }
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+}
