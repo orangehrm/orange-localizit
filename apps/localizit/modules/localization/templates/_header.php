14b377f2c0f74866d76ab015b65fe430efd194ff
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-07 10:13:16 +0000

    partial login

--- apps/localizit/modules/localization/templates/_header.php
+++ apps/localizit/modules/localization/templates/_header.php
@@ -5,7 +5,7 @@
     <div class="normalText float-right logout">
        <?php if($sf_user->isAuthenticated()) echo link_to( __('logout', null, 'localizationMessages'), 'authentication/logout'); ?>
     </div>
-    <div class="normalText float-right">
+    <div class="normalText float-right logout">
         <?php if($sf_user->isAuthenticated()) { echo __('welcome', null, 'localizationMessages'); ?> &nbsp;<span class="boldText"><?php echo '&lsquo;' . $sf_user->getAttribute('username') . '&rsquo;' ?> </span> <?php } ?>
         <?php if(!$sf_user->isAuthenticated()) { echo link_to( __('login', null, 'authenticationMessages'), '@sign_in'); } ?>
     </div>
