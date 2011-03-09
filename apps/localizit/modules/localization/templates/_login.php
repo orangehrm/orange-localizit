008981939033b8c640101845d7392ea31892d853
chameeraCD@gmail.com <chameeraCD@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-09 03:30:33 +0000

    ui mods

--- apps/localizit/modules/localization/templates/_login.php
+++ apps/localizit/modules/localization/templates/_login.php
@@ -1,9 +1,9 @@
 <form action="<?php echo url_for('@sign_in'); ?>" method="post" id="sign_in_form" name="sign_in_form">
     <?php
-    $form = new SignInForm();
-    echo $form['_csrf_token']->render();
+    $signInForm = new SignInForm();
+    echo $signInForm['_csrf_token']->render();
     ?>
-     <?php echo __('username', null, 'authenticationMessages'); ?><input class="text_input" type="text" name="sign_in[loginName]" id="sign_in_loginName"/>
-    <?php echo __('password', null, 'authenticationMessages'); ?><input class="text_input" type="password" name="sign_in[password]" id="sign_in_password"/>
-    <input type="button" name="login_label" id="login" class="button normalText" value="<?php echo __('login', null , 'authenticationMessages') ?>" />
+    <?php echo $signInForm['loginName']->renderLabel( __('username', null, 'authenticationMessages')); echo $signInForm['loginName']->render(); ?>
+    <?php echo $signInForm['password']->renderLabel( __('password', null, 'authenticationMessages')); echo $signInForm['password']->render(); ?>
+    <input type="button" name="login_label" id="login" class="button normalText" value="<?php echo __('login', null , 'authenticationMessages'); ?>" />
 </form>
