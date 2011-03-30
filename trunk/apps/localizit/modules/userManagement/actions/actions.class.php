1c517e6b75d3c226ba3e19a1bad6248b4212a461
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-30 12:12:34 +0000

    Modifications

--- trunk/apps/localizit/modules/userManagement/actions/actions.class.php
+++ trunk/apps/localizit/modules/userManagement/actions/actions.class.php
@@ -16,73 +16,80 @@
  * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
  * Boston, MA  02110-1301, USA
  */
+class userManagementActions extends sfActions {
 
-class userManagementActions extends sfActions
-{
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->users = Doctrine::getTable('User')
-      ->createQuery('a')
-      ->execute();
-  }
+    private $userManagementService;
 
-  public function executeShow(sfWebRequest $request)
-  {
-    $this->user = Doctrine::getTable('User')->find(array($request->getParameter('user_id')));
-    $this->forward404Unless($this->user);
-  }
+    /**
+     * This method is executed before each action
+     */
+    public function preExecute() {
 
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new UserForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new UserForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->forward404Unless($user = Doctrine::getTable('User')->find(array($request->getParameter('user_id'))), sprintf('Object user does not exist (%s).', $request->getParameter('user_id')));
-    $this->form = new UserForm($user);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($user = Doctrine::getTable('User')->find(array($request->getParameter('user_id'))), sprintf('Object user does not exist (%s).', $request->getParameter('user_id')));
-    $this->form = new UserForm($user);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    //$request->checkCSRFProtection();
-
-    $this->forward404Unless($user = Doctrine::getTable('User')->find(array($request->getParameter('user_id'))), sprintf('Object user does not exist (%s).', $request->getParameter('user_id')));
-    $user->delete();
-
-    $this->redirect('userManagement/index');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $user = $form->save();
-
-      $this->redirect('userManagement/edit?user_id='.$user->getUserId());
+        $this->userManagementService = $this->getUserManagementService();
     }
-  }
+
+    /**
+     * Get Localization Service
+     */
+    public function getUserManagementService() {
+        $this->userManagementService = new UserManagementService();
+        $this->userManagementService->setUserManagementDao(new UserManagementDao());
+        return $this->userManagementService;
+    }
+
+    public function executeIndex(sfWebRequest $request) {
+        $this->users = Doctrine::getTable('User')
+                        ->createQuery('a')
+                        ->execute();
+    }
+
+    public function executeShow(sfWebRequest $request) {
+        $this->user = Doctrine::getTable('User')->find(array($request->getParameter('user_id')));
+        $this->forward404Unless($this->user);
+    }
+
+    public function executeNew(sfWebRequest $request) {
+        $this->form = new UserForm();
+    }
+
+    public function executeCreate(sfWebRequest $request) {
+        $this->forward404Unless($request->isMethod(sfRequest::POST));
+
+        $this->form = new UserForm();
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('new');
+    }
+
+    public function executeEdit(sfWebRequest $request) {
+        $this->forward404Unless($user = Doctrine::getTable('User')->find(array($request->getParameter('user_id'))), sprintf('Object user does not exist (%s).', $request->getParameter('user_id')));
+        $this->form = new UserForm($user);
+    }
+
+    public function executeUpdate(sfWebRequest $request) {
+        $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
+        $this->forward404Unless($user = Doctrine::getTable('User')->find(array($request->getParameter('user_id'))), sprintf('Object user does not exist (%s).', $request->getParameter('user_id')));
+        $this->form = new UserForm($user);
+
+        $this->processForm($request, $this->form);
+
+        $this->setTemplate('edit');
+    }
+
+//    public function executeDelete(sfWebRequest $request) {
+//        //$request->checkCSRFProtection();
+//        $this->userManagementService->deleteUser($request['user_id']);
+//        $this->redirect('@userManagement');
+//    }
+
+    protected function processForm(sfWebRequest $request, sfForm $form) {
+        $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
+        if ($form->isValid()) {
+            $user = $form->save();
+
+            $this->redirect('userManagement/edit?user_id=' . $user->getUserId());
+        }
+    }
+
 }
