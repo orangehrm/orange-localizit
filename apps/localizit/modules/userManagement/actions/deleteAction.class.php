eac7fadc2afa1b54058c8c27f94f07aa77f06605
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-03-30 12:13:12 +0000

    Adding Pages

---
+++ apps/localizit/modules/userManagement/actions/deleteAction.class.php
@@ -0,0 +1,43 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of deleteAction
+ *
+ * @author waruni
+ */
+class deleteAction extends sfAction {
+
+    private $userManagementService;
+
+    /**
+     * This method is executed before each action
+     */
+    public function preExecute() {
+
+        $this->userManagementService = $this->getUserManagementService();
+    }
+
+    /**
+     * Get Localization Service
+     */
+    public function getUserManagementService() {
+        $this->userManagementService = new UserManagementService();
+        $this->userManagementService->setUserManagementDao(new UserManagementDao());
+        return $this->userManagementService;
+    }
+    /**
+     * Delete User .
+     * @param <type> $request
+     */
+
+    public function execute($request) {
+        $this->userManagementService->deleteUser($request['user_id']);
+        $this->redirect('@userManagement');
+    }
+
+}
+
