87e0a11580c40ac397b936ad855b966faf9c4434
waruniwickramanayake@gmail.com <waruniwickramanayake@gmail.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2011-04-06 07:59:51 +0000

    Adding Forms

---
+++ trunk/apps/localizit/modules/localization/templates/_LanguageGroupList.php
@@ -0,0 +1,19 @@
+<select class="langueDropDownList" id="add_label_language_group_id" name="<?php if(isset($isArray)) {?>languageGroupList<?php echo "_".$prefix."[]";
+        }else {?>add_label[language_group_id]<?php }?>">
+            <?php if($languageGroupList) {?>
+                <?php foreach ($languageGroupList as $langGroup) { ?>
+                    <?php if(!isset($selected_index)) {?>
+                        <?php if($langGroup->getId() !=0) {?>
+
+    <option value="<?php echo $langGroup->getId() ?>">
+            <?php echo  $langGroup->getGroupName() ?>
+    </option>
+                    <?php }?>
+                <?php }else {?>
+    <option value="<?php echo $langGroup->getId() ?>" <?php echo $selected_index==$langGroup->getId() ? 'selected="selected"':null?>>
+                    <?php echo $langGroup->getGroupName() ?>
+    </option>
+                <?php }?>
+            <?php }?>
+        <?php }?>
+</select>
