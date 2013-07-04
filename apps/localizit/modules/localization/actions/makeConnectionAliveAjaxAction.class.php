f8074624554e48a7a3c7dab1bd343f4322bfc6bf
Jathu@orangehrm.us.com <Jathu@orangehrm.us.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2013-07-04 10:17:03 +0000

    New action file added to make connection live with user whil translation happening

---
+++ apps/localizit/modules/localization/actions/makeConnectionAliveAjaxAction.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of makeConnectionAliveAjaxAction
+ *
+ * @author jathu
+ */
+class makeConnectionAliveAjaxAction {
+     public function execute($request) {
+        
+        echo 'success';
+        return sfView::NONE;
+    }
+
+}
+
+?>
