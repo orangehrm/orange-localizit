ea2b98544ed633e91a08ca98b48cd7fec5bc2741
shermal@orangehrm.us.com <shermal@orangehrm.us.com@203a0707-46dc-ec3c-4c80-38feb700b65f>
2014-03-13 10:17:16 +0000

    Changes to compatible with production db

--- upgrade/upgrade.sql
+++ upgrade/upgrade.sql
@@ -3,3 +3,5 @@ SET @general_id := (SELECT `id` FROM `ohrm_group` WHERE `name` ='General' LIMIT 
 UPDATE `ohrm_source` SET `group_id`=@general_id WHERE NOT `group_id` = @general_id;
 
 DELETE FROM `ohrm_group` WHERE NOT `id` = @general_id;
+
+DELETE FROM `ohrm_source` WHERE `id` NOT IN (SELECT * FROM (SELECT MIN(s.`id`) FROM `ohrm_source` s GROUP BY BINARY s.`value`) x);
